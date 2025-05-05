2833031bcae0aafe68affddc75904448c7282334
tungnguyenvanthanh@hotmail.com <Nguyn Vn Thanh Tng>
2025-05-05 11:32:46 +0700

    layout

---
+++ SourceBaseCsharp/AppServer/PageControllers/Shared/Components/MainHeaderViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppServer.PageControllers.Shared.Components
+{
+    public class MainHeaderViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult(View("~/Views/Shared/ViewComponents/_MainHeader.cshtml"));
+        }
+    }
+}
